20662a9332a7a438252b1ee4a9dafe1453549011
Valeria Caro Ramirez <v.caro@uniandes.edu.co>
2021-12-12 03:11:05 -0500

    added README

--- src/Learn/HelloWorld.java
+++ src/Learn/HelloWorld.java
@@ -1,4 +1,20 @@
 package Learn;
 
 public class HelloWorld {
+    /* Repeat it 10 times using while */
+    public static void main(String[] args) {
+        /* While Loop */
+        int i = 0;
+        while (i < 2) {
+            System.out.println("Hello World!");
+            i++; /* i+=1 */
+        /* For Loop */
+        for (int j = 0; j < 2; j++) { /* Se define (variable; condicion; incremento) */
+            System.out.println("Hello World!");
+        }
+
+        }
+    }
+
+
 }
